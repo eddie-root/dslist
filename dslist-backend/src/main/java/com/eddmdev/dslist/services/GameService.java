804b527962cf78d8ac9bfda53a64b20ff478eaef
Admilson Ferreira <ferreiraad3@gmail.com>
2023-11-18 05:48:36 -0300

    Get game by id

--- dslist-backend/src/main/java/com/eddmdev/dslist/services/GameService.java
+++ dslist-backend/src/main/java/com/eddmdev/dslist/services/GameService.java
@@ -3,11 +3,14 @@ package com.eddmdev.dslist.services;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.stereotype.Service;
 
+import com.eddmdev.dslist.dto.GameDTO;
 import com.eddmdev.dslist.dto.GameMinDTO;
 import com.eddmdev.dslist.entities.Game;
 import com.eddmdev.dslist.repositories.GameRepository;
+
 
 @Service
 public class GameService {
@@ -19,6 +22,13 @@ public class GameService {
 		List<Game> result = gameRepository.findAll();
 		List<GameMinDTO> dto = result.stream().map(x -> new GameMinDTO(x)).toList();
 		return dto;
+	}
+	
+	@Transactional(readOnly = true)
+	public GameDTO findById(Long id) {
+		Game result = gameRepository.findById(id).get();
+		GameDTO dto = new GameDTO(result);
+		return dto;		
 	}
 
 }
